a69c0ac75d8034025ccb28d3a11015fa5a147247
nicocag29 <nicolas.cagliero@mi.unc.edu.ar>
2024-04-11 20:31:07 -0300

    juju grilla top ananananana

--- src/Dibujos/Grilla.hs
+++ src/Dibujos/Grilla.hs
@@ -1,8 +1,18 @@
 module Dibujos.Grilla (
-    grilla
+    grilla, corConf
 ) where
 
-import Dibujo (Dibujo, juntar, apilar)
+import Dibujo (Dibujo, figura, juntar, apilar, rot45, rotar, encimar, espejar)
+import FloatingPic(Conf(..), Output, half, zero)
+import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+import Graphics.Gloss ( Picture , blue, red, color, line, pictures, text )
+import Graphics.Gloss.Data.Picture(translate , scale)
+import Dibujos.Feo (testAll)
+
+type Coordenada = (Int, Int)
+
+interpCor :: Output Coordenada
+interpCor (x, y) d w h = translate (fst d) (snd d) (text ( "(" ++ show x ++ "," ++ show y ++ ")" ))
 
 row :: [Dibujo a] -> Dibujo a
 row [] = error "row: no puede ser vacío"
@@ -16,3 +26,16 @@ column (d:ds) = apilar (fromIntegral $ length ds) 1 d (column ds)
 
 grilla :: [[Dibujo a]] -> Dibujo a
 grilla = column . map row
+
+testAllCor :: Dibujo Coordenada
+testAllCor = grilla [ [figura (0, 0), figura (0, 1), figura (0,2)],
+                      [figura (1, 1), figura (1, 2), figura (1,3)]
+                    ]
+
+
+corConf :: Conf
+corConf = Conf {
+    name = "Grilla"
+    , pic = testAllCor
+    , bas = interpCor
+}
