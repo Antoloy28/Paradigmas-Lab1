85477fbc4ae6896dff79c9032374ab9ddcb0726f
Maurillugdar <maurillugdar@gmail.com>
2024-04-11 11:48:42 -0300

    Interp.hs completed

--- src/Interp.hs
+++ src/Interp.hs
@@ -8,6 +8,8 @@ import Dibujo
 import FloatingPic
 import Graphics.Gloss (Display (InWindow), color, display, makeColorI, pictures, translate, white, Picture)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+import Graphics.Gloss.Data.Vector (Vector, mulSV)
+
 
 -- Dada una computación que construye una configuración, mostramos por
 -- pantalla la figura de la misma de acuerdo a la interpretación para
@@ -24,36 +26,50 @@ initial (Conf n dib intBas) size = display win white $ withGrid fig size
 
 -- Interpretación de (^^^)
 ov :: Picture -> Picture -> Picture
-ov f g d w h =  pictures[p, q]
+ov p q =  pictures [p, q]
 
 r45 :: FloatingPic -> FloatingPic
-r45 f d w h = f (d+(w+h)/2) ((w+h)/2) ((h-w)/2)
-
+r45 f d w h = f d_f w_f h_f
+            where
+              d_f = d V.+ mulSV 0.5 (w V.+ h)
+              w_f = mulSV 0.5 (w V.+ h)
+              h_f = mulSV 0.5 (h V.- w)
 rot :: FloatingPic -> FloatingPic
-rot f d w h = f (d + w) h (-w)
+rot f d w h = f d_f h h_f
+            where
+              d_f = d V.+ w
+              h_f = mulSV (-1) w
 
 esp :: FloatingPic -> FloatingPic
-esp f d w h = f (d+w) (-w) h
+esp f d w = f d_f w_f
+            where
+              d_f = d V.+ w
+              w_f = mulSV (-1) w
 
 sup :: FloatingPic -> FloatingPic -> FloatingPic
-sup f g d w h =  pictures[f d w h, g d w h]
+sup f g d w h =  pictures [f d w h, g d w h]
 
 jun :: Float -> Float -> FloatingPic -> FloatingPic -> FloatingPic
-jun m n f g d w h = pictures[f d w' h, g (d+w') (r'*w) h]
+jun m n f g d w h = pictures [f d w' h, g d_g w_g h]
                 where
                     r' = n/(m+n)
                     r  = m/(m+n)
-                    w' = r*w
+                    w' = mulSV r w
+                    w_g = mulSV r' w
+                    d_g = d V.+ w'
 
 api :: Float -> Float -> FloatingPic -> FloatingPic -> FloatingPic
-api m n f g d w h = pictures[f (d+h') w (r*h), g d w h']
+api m n f g d w h = pictures [f d_f w h_f, g d w h']
                 where
                     r' = n/(m+n)
                     r  = m/(m+n)
-                    h' = r'*w
+                    h' = mulSV r' w
+                    h_f = mulSV r h
+                    d_f = d V.+ h'
 
--- (a -> FloatingPic) -> (Dibujo a -> Picture) = (a -> FloatingPic) -> Dibujo a -> Picture
+-- (a -> FloatingPic) -> (Dibujo a -> FloatingPic) -> Picture = (a -> FloatingPic) -> Dibujo a -> Picture
 -- Currificación P => (Q => R) = (P ^ Q => R) 
 
 interp :: Output a -> Output (Dibujo a)
-interp b dib = foldDib b rot t45 esp api jun sup dib 
+interp b = foldDib b rot r45 esp api jun sup
+
